6ce747f94b216c8e72740a627de16c17848e87f1
Gustavo Henrique Lima Silva <78971590+guhls@users.noreply.github.com>
2022-04-20 10:40:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request
 from utils import braille
 
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     arrays = braille()
 
-    keys_arrays = arrays.keys() 
+    keys_arrays = arrays.keys()
 
-    phrase = ''
-    if request.method == 'POST':
-        phrase = request.form['phrase']
-        
-        return render_template('index.html', arrays=arrays, phrase=phrase, keys_arrays=keys_arrays)    
+    phrase = ""
+    if request.method == "POST":
+        phrase = request.form["phrase"]
 
-    return render_template('index.html', arrays=arrays, keys_arrays=keys_arrays)
+        return render_template(
+            "index.html", arrays=arrays, phrase=phrase, keys_arrays=keys_arrays
+        )
+
+    return render_template("index.html", arrays=arrays, keys_arrays=keys_arrays)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
